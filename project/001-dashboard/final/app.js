78388bb67704502cab3cbd4b075b92f8a1489024
pleasantvik <adedayo.ayoola5683@gmail.com>
2023-12-31 10:29:57 +0100

    project added

---
+++ project/001-dashboard/final/app.js
@@ -0,0 +1,17 @@
+const inputEl = document.querySelector("input");
+const light = document.querySelector("body");
+const highlight = document.querySelectorAll(".highlight");
+const card = document.querySelectorAll(".card");
+
+// inputEl.addEventListener("click", () => {
+//   if (light.classList.contains("light")) {
+//     light.classList.remove("light");
+//     highlight.forEach((el) => el.classList.remove("light"));
+//     card.forEach((el) => el.classList.remove("light"));
+//   } else {
+//     light.classList.add("light");
+//     card.forEach((el) => el.classList.add("light"));
+
+//     highlight.forEach((el) => el.classList.add("light"));
+//   }
+// });
